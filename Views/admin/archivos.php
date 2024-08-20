a54f35167396987ae2a1531e1dca01e7d87c83f6
Ronal280300 <rosepa2803@gmail.com>
2024-08-20 13:39:09 -0600

    Se elimina boton de Archivos

--- Views/admin/archivos.php
+++ Views/admin/archivos.php
@@ -5,16 +5,12 @@
     <div class="content-wrapper">
         <div class="container-fluid">
             <div class="row">
-                <div class="col d-flex justify-content-between align-items-center">
+                <div class="col">
                     <div class="page-description d-flex align-items-center">
                         <div class="page-description-content flex-grow-1">
                             <h1><?php echo $data['carpeta']['nombre']; ?></h1>
                         </div>
                     </div>
-                    <!-- Botón Cargar Archivos -->
-                    <button type="button" id="btnSubirArchivo" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#modalArchivos">
-                        <i class="material-icons">upload_file</i> Cargar Archivos
-                    </button>
                 </div>
             </div>
             <div class="row">
